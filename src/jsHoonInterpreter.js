640424327562b1f87807dc684cff85043099bf59
Ṅero Moskalyk <versus.energy@protonmail.com>
2026-06-04 19:40:29 -0400

    interpreter (lazy) additional gates and core logic

--- src/jsHoonInterpreter.js
+++ src/jsHoonInterpreter.js
@@ -2,6 +2,12 @@ const gates = {
     'add': (arg1, arg2) => {
       return parseInt(arg1) + parseInt(arg2)
     },
+    'mul': (arg1, arg2) => {
+      return parseInt(arg1) * parseInt(arg2)
+    },
+    'gth': (arg1, arg2) => {
+        return parseInt(arg1) >= parseInt(arg2)
+    },
     '@': (arg) => {
       return arg
     }
@@ -13,18 +19,229 @@ const stairs = `
         }
     }
 `
-const runes = [':-', '^-']
+const runes = [':-', '^-', '|=', '=/', '|-', '?:', '%=', '$']
+
 const caseByCase = (rune) => `case '${rune}': ${stairs} break;`
 const runeRunner = eval(`const appendage = (rune) => {switch(rune){${runes.reduce((initial, rune) => {
     return initial + caseByCase(rune)
 }, '')}}}; const returnFunc = () => appendage; returnFunc()`)
 
-const compiler = (hoon, depth, verbose) => {
+const wait = (ms) => new Promise((res) => setTimeout(res, ms))
+const compiler = (hoon, depth, verbose, args) => {
     try {
         const isRune = hoon.slice(0,2)
         const pattern = /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)/;
+        if(runes.includes(isRune) && isRune == runes[2]){
+            const pattern2Cells = /(.*)\[((.+)(?=\=)=(.+))+\s(.*)=(.*)\](.*)/; // (?<rune>\S+)\s{2}\[((?<var>.*?)(?=\=)(.*)]*)\]
+            const pattern1Atom = /((=\/.+)\s{2})(?=\?:)(.+)/; // (?<rune>\S+)\s{2}\[((?<var>.*?)(?=\=)(.*)]*)\]
 
-        if(runes.includes(isRune)){
+            const match = hoon.match(pattern2Cells)
+            let match1 = hoon.match(pattern1Atom)
+             const functionName = (a, b) => {
+                const bounding = /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)/
+                const match1 = hoon.match(bounding)
+                let result;
+                
+                result = runeRunner(match1.groups.rune)(match1.groups.func)([a, b])
+                if(result) return a
+                else return b
+            }
+            
+            const functionName1Atom = (a) => {
+                                         // (.+)\s{2}(.+)\s{2}(.+)\s{2}(?=\?|=\/:)(.+)
+                const localBounding = /(.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)/
+                let matchAtoms
+                let checkForLocal = true
+                let vars = []
+                let i = 0;
+                matchAtoms = hoon.match(localBounding)
+                
+                const inputs = matchAtoms[3].trim().split('=')
+                const isCenTis = matchAtoms
+                let vars2 = {}
+                
+                // (.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)
+                // |=  a=@ud  =/  b  1  ?:  :-  gth  a  b  a  b
+
+                // ((\S+)\s{2}(\S+)\s{2}(\S+))+\s*?
+                // =/  a  2  =/  b  1  ?:  :-  gth  a  b  a  b
+
+                // /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)/
+                // ?:  :-  gth  a  b  a  b(?=\?:)
+                const firstMatch = matchAtoms[0].match(/(.+)\s{2}\|-\s{2}(.+)\s{2}((?=%=)(.+)\s{2}(?=\=))+(.*)==/)
+                    
+                    // console.log(firstMatch)
+                    
+                    // if(!firstMatch){
+                    // console.log('matchAtoms[0]')
+                    // console.log(matchAtoms[0])
+                    //     const matchedWork = matchAtoms[0].match(/(.*)\s{2}((.+)(?=\=)(.+)\s{2}(?=\?:))+(.*)/)
+                        
+                    //     // (.*)\s{2}((.+)(?=\=)(.+)\s{2}(?=\?:))+(.*)
+                    //     console.log(matchedWork[5].match(/(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)/))
+                    //     // return  
+                if(vars2 != {}){
+                // return 11
+                    // entry
+
+                    // 1.
+                    // (.+)\s{2}\|-\s{2}(.+)\s{2}((?=%=)(.+)\s{2}(?=\=))+(.*)==
+                    // |=  a=@ud  =/  b  4  |-  ?:  :-  gth  a  b  counter  b  %=  $  counter  1  b  2  ==
+                    const firstMatch = matchAtoms[0].match(/(.+)\s{2}\|-\s{2}(.+)\s{2}((?=%=)(.+)\s{2}(?=\=))+(.*)==/)
+                    
+                    // console.log(firstMatch)
+                    
+                    // console.log('matchAtoms[0]')
+                    // console.log(matchAtoms[0])
+                    //     const matchedWork = matchAtoms[0].match(/(.*)\s{2}((.+)(?=\=)(.+)\s{2}(?=\?:))+(.*)/)
+                        
+                    //     // (.*)\s{2}((.+)(?=\=)(.+)\s{2}(?=\?:))+(.*)
+                    //     console.log(matchedWork[5].match(/(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)/))
+                    //     return  
+                        
+                    // 2.
+                    // (.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)
+                    // |=  a=@ud  =/  b  4
+                    const secondMatch = firstMatch[1].match(/(.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)/)
+                    const inputsSecondMatch = secondMatch[3].trim().split('=')
+                    const inputsSecondMatch1 = secondMatch[4].trim().split('  ')
+                    const inputsSecondMatch2 = secondMatch[5].trim().split('  ')
+                    
+                    // small hack
+                    // process.exit();
+                    vars2[inputsSecondMatch1[1]] = inputsSecondMatch1[2]
+                    vars2[inputsSecondMatch2[1]] = inputsSecondMatch2[2]
+                    const secondIshMatch = ((firstMatch[2] + "  " + firstMatch[3]) + "==").match(/(.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)/)
+
+                    // 3.
+                    // /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)/
+                    // ?:  :-  gth  a  b  a  b
+                    // const thirdMatch = firstMatch[2].match(/(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)\s{2}(?<arg4>.*)/)
+                    // console.log('thirdMatch')
+                    // console.log(thirdMatch)
+                    
+                    // const bounding = /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)\s{2}(?<arg4>.*)/
+                    // const matches = matchAtomsNonInputs[5].match(bounding)
+                    // console.log(matches)
+
+                    // 4.
+                    // \$\s{2}(?<assign>(((\S+)\s{2}(\S+)))\s{2})*
+                    // %=  $  counter  1  b  2  ==
+                    let fourthMatch = firstMatch[3].match(/\$\s{2}(?<assign>(((\S+)\s{2}(\S+)))\s{2})*/)
+                    let checker = false
+                    let updatedMatch
+                    updatedMatch = fourthMatch[0]
+                                        
+                    // while(!checker){
+                    //     updatedMatch = updatedMatch.match(/\$\s{2}(?<assign>(((\S+)\s{2}(\S+)))\s{2})*/)
+                    //     console.log('updatedMatch ---')
+                    //     console.log(updatedMatch)
+                    //     // updatedMatch = updatedMatch.match(/\$\s{2}(?<assign>(((.*)\s{2}(.*)\s{2}(.*)\s{2}(\S+)\s{2}(\S+)))\s{2})*/)
+                    //     if(updatedMatch[0].trim() == runes[7]) break;
+                    //     console.log(updatedMatch)
+                    //     vars2[updatedMatch[4]] = updatedMatch[5]
+                    //     updatedMatch = updatedMatch[0].replace(updatedMatch[2], '')
+                    // }
+                    
+                    /////////////////////
+                
+                    vars2[inputsSecondMatch[0]] = a
+                        
+                    const thirdMatch = secondIshMatch[1].match(/(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)(?<arg4>.*)/)
+                    
+                    while(!runeRunner(thirdMatch.groups.rune)(thirdMatch.groups.func)([vars2[thirdMatch.groups.arg1], vars2[thirdMatch.groups.arg2]])){
+                        const secondIshMatch = ((firstMatch[2] + "  " + firstMatch[3]) + "==").match(/(.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)/)
+                        
+                        const inbetween = secondIshMatch[0].match(/(?<rune>\S+)\s{2}(?<func>\w+)(\s{2}(?<arg>\S+))?\s{2}%=\s{2}(?<arg3>.*)/)
+                                            
+                        while(true){
+                        updatedMatch = fourthMatch[0]
+                            const secondIshMatch = ((firstMatch[2] + "  " + firstMatch[3]) + "==").match(/(.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)/)
+                            updatedMatch = secondIshMatch[2].match(/\$\s{2}(?<func>\w+)\s{2}(?<rune>\S+)(\s{2}(?<arg2>\S+)\s{2}(\S+)*)\s{2}(\S+)\s{2}/)
+
+                           
+                            let compute1;
+                            compute1 = vars2[updatedMatch[5]]
+                            
+                            if(updatedMatch[5] in vars2) {
+                                compute1 = vars[updatedMatch[5]]
+                            } else {
+                                compute1 = updatedMatch[5]
+                            }
+                            let compute2;
+                        
+                             if(updatedMatch[6] in vars2) {
+                                compute2 = vars2[updatedMatch[6]]
+                            } else {
+                               compute2 = updatedMatch[6]
+                            }
+                            
+                            
+                            // await wait(1000)
+                            vars2[updatedMatch[1]] = runeRunner(updatedMatch.groups.rune)(updatedMatch.groups.arg2)([compute1, compute2])
+                            if(updatedMatch[0].trim() == runes[7]) break;
+                            
+                            vars2[updatedMatch[7]] = updatedMatch[8]
+                            secondIshMatch[2] = secondIshMatch[2].replace('%=  $  '+ updatedMatch[1] +'  '+ updatedMatch[2] + updatedMatch[3] + '  ' + updatedMatch[6], '')
+                            vars2[secondIshMatch[2].trim().split(' ')[0]] = secondIshMatch[2].trim().split(' ')[2]
+                            if(runeRunner(thirdMatch.groups.rune)(thirdMatch.groups.func)([vars2[thirdMatch.groups.arg1], vars2[thirdMatch.groups.arg2]])) break;
+                        }
+                        
+                        break;
+                    }
+                    
+                    return vars2[thirdMatch.groups.arg3]
+
+                } else {
+                    
+                    const localBounding2 = /((\S+)\s{2}(\S+)\s{2}(\S+))\s*(.*)/
+
+                    let matchAtomsNonInputs = matchAtoms[0].replace(matchAtoms[1], '').replace(matchAtoms[3].trim(), '').trim().match(localBounding2)
+                    
+                    const vars1 = {}
+                                                vars1[matchAtomsNonInputs[3]] = matchAtomsNonInputs[4] 
+
+                let lastCheck = false
+                while(!lastCheck){
+                    const localBounding2 = /((\S+)\s{2}(\S+)\s{2}(\S+))\s*(.*)/
+
+                    let atomsNonInputs = matchAtomsNonInputs[0].replace(matchAtomsNonInputs[1], '')
+                    matchAtomsNonInputs = atomsNonInputs.match(localBounding2)
+                    // lastCheck = true
+                    if(matchAtomsNonInputs[5].slice(0,2) != runes[3]){
+                                                                    vars1[matchAtomsNonInputs[3]] = matchAtomsNonInputs[4] 
+
+                        checkForLocal = true
+                        break;
+
+                    } else {
+                        vars1[matchAtomsNonInputs[3]] = matchAtomsNonInputs[4] 
+                    }
+                }
+                
+                let result;
+                console.log(vars)
+                const bounding = /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)\s{2}(?<arg4>.*)/
+                const matches = matchAtomsNonInputs[5].match(bounding)
+                const funcRegex = /(.+)\s{2}(.+)/
+
+                vars1[inputs[0]] = a
+                result = runeRunner(matches.groups.rune)(matches.groups.func)([vars1[matches.groups.arg1], vars1[matches.groups.arg2]])
+                if(result) return vars1[matches.groups.arg3]
+                else return vars1[matches.groups.arg4]
+                                }
+
+            }
+            
+            if(match1 != null){
+                return functionName1Atom
+            } else {
+                return functionName
+
+            }
+
+        } else if(runes.includes(isRune)){
+            
             const match = hoon.match(pattern)
             const isRuneArg1 = match.groups.arg1.slice(0,2)
             const isRuneArg2 = match.groups.arg2.slice(0,2)
@@ -58,11 +275,44 @@ let hoon1 = ":-  add  1  2";
 let hoon2 = ":-  add  1  :-  add  1  2";
 let hoon3 = ":-  add  1  :-  add  :-  add  1  :-  add  1  2";// <|>
 let hoon5 = ":-  add  :-  add  1  2  2";
+let hoon6 = ":-  gth  3  2"
+let hoon7 = "|=  [a=@ud b=@ud]  ?:  :-  gth  a  b  a  b"
+
+let hoon8 = "|=  a=@ud  =/  counter  1  =/  sum  0  |-  ?:  :-  gth  counter  a  sum  %=  $  counter  :-  add  counter  1  sum  :-  add  counter  sum  =="
+let hoon9 = "|=  a=@ud  =/  b  4  ?:  :-  gth  a  b  a  b"
+let hoon10 = "|=  a=@ud  =/  b  4  =/  sum  0  ?:  :-  gth  a  b  sum  b"
+let hoon11 = "|=  a=@ud  =/  b  4  =/  counter  0  |-  ?:  :-  gth  counter  a  counter  %=  $  counter  1  b  2  =="
+let hoon12 = "|=  a=@ud  =/  b  4  =/  counter  0  |-  ?:  :-  gth  counter  a  counter  %=  $  counter  :-  add  1  counter  b  2  =="
+
+// trap entry
+
+// 1.
+// (.+)\s{2}\|-\s{2}(.+)\s{2}((?=%=)(.+)\s{2}(?=\=))+(.*)==
+// |=  a=@ud  =/  b  4  |-  ?:  :-  gth  a  b  counter  b  %=  $  counter  1  b  2  ==
+
+// 2.
+// (.*)\s{2}((.+)(?=\=)=(.+)\s{2}(?=\=))+(.*)
+// |=  a=@ud  =/  b  4
+
+// 3.
+// /(?<rune>\S+)\s{2}(?<func>\w+)\s{2}(?<arg1>.*)\s{2}(?<arg2>.*)\s{2}(?<arg3>.*)/
+// ?:  :-  gth  a  b  a  %=  $  counter  1  b  2  ==
+
+// 4.
+// \$\s{2}(?<assign>(((\S+)\s{2}(\S+)))\s{2})*
+// %=  $  counter  1  b  2  ==
 
 const verbose = false
-console.log(3 == compiler(hoon1, 0, verbose)) // true
-console.log(4 == compiler(hoon2, 0, verbose)) // true
-console.log(5 == compiler(hoon3, 0, verbose)) // true
-console.log(5 == compiler(hoon5, 0, verbose)) // true
+console.log(5===compiler(hoon5, 0, verbose))
+console.log(2===compiler(hoon12, 0, verbose)(2))
+console.log(3===compiler(hoon7, 0, verbose)(2,3)) // true
 
-module.exports = {compiler}
+// TODO: tests and refactor
+// console.log(1===compiler(hoon11, 0, verbose)(1))/ true
+// console.log(compiler(hoon9, 0, verbose)(5)) // TODO: abstract and fix
+// console.log(compiler(hoon10, 0, verbose)(5))/ true
+
+console.log(10==compiler(hoon12, 0, verbose)(10))/ true
+
+let hoon13 = "|=  a=@ud  =/  counter  2  =/  b  2  |-  ?:  :-  gth  counter  a  counter  %=  $  counter  :-  mul  2  counter  =="
+console.log(16==compiler(hoon13, 0, verbose)(10))/ true
